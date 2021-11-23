587568c9e62feb696b4911ca80399dbf879040d6
mariok90 <mario90@mailbox.tu-berlin.de>
2021-11-23 14:03:21 +0100

    restruct of type dispatch

--- src/Mask.jl
+++ src/Mask.jl
@@ -3,100 +3,149 @@ Timestep,
 Variable, 
 Carrier, 
 Region,
+Scenario
 Mask,
-PivotResult
+PivotResult,
+pivotresult
 
-abstract type ResultDimension end
+const colnames = (
+    Region = "region_dispatch_",
+    Technology = "technology_",
+    Timestep = "timestep_superordinate_dispatch_",
+    Carrier = "carrier_",
+    Variable = "variable",
+    Scenario = "scenario"
+)
 
-for dim = (:Technology, :Timestep, :Variable, :Carrier, :Region, :Scenario)
+abstract type ResultDimension{T} end
+abstract type ResultWithDimension{T} <: ResultDimension{T} end
+abstract type ResultWithoutDimension{T} <: ResultDimension{T} end
+const SuperType = Union{AbstractVector, AbstractString, Nothing}
+
+
+for dimension = (:Technology, :Timestep, :Carrier, :Region)
     eval(quote
-        struct $dim <: ResultDimension
-            pairs
+        struct $dimension{T<:SuperType} <: ResultWithDimension{T}
+            vals::T
             mode::Symbol
             dim::Int
+            colname::AbstractString
         
-            $dim() = new(nothing, :filter, 1)
-            function $dim(args::Pair...; mode = :equal, dim=0)
-                if dim == 0
-                    dim = getindex.(args, 1) |> first |> maximum
-                end
-
-                return new([args...], mode, dim)
+            function $dimension()
+                colname = colnames.$dimension * "1"
+                return new{Nothing}(nothing, :equal, 1, colname)
+            end
+            
+            function $dimension(p::T; mode = :in) where T<:Pair{<:Int,<:AbstractVector}
+                dim, val = p
+                colname = colnames.$dimension * "$dim"
+                return new{typeof(val)}(val, mode, dim, colname)
             end
 
-            function $dim(args::T...; mode = :equal, dim=1) where T<:AbstractString
-                return new([args...], mode, dim)
+            function $dimension(p::T; mode = :equal) where T<:Pair{<:Int,<:AbstractString}
+                dim, val = p
+                colname = colnames.$dimension * "$dim"
+                return new{typeof(val)}(val, mode, dim, colname)
             end
 
-            function $dim(i::T; mode = :equal, dim=1) where T<:Integer
-                return new(nothing, mode, i)
+            function $dimension(x::T; mode = :equal, dim=1) where T<:AbstractString
+                colname = colnames.$dimension * "$dim"
+                return new{T}(x, mode, dim, colname)
+            end
+
+            function $dimension(i::T; mode = :equal) where T<:Integer
+                colname = colnames.$dimension * "$i"
+                return new{Nothing}(nothing, mode, i, colname)
+            end
+
+            function $dimension(vec::T; mode = :in, dim=1) where T<:AbstractVector
+                colname = colnames.$dimension * "$dim"
+                return new{T}(vec, mode, dim, colname)
+            end
+
+            function $dimension(args::T...; mode = :in, dim=1) where T<:AbstractString
+                colname = colnames.$dimension * "$dim"
+                vec = [args...]
+                return new{typeof(vec)}(vec, mode, dim, colname)
+            end
+        end
+    end)
+end
+
+for dimension = (:Variable, :Scenario)
+    eval(quote
+        struct $dimension{T<:SuperType} <: ResultWithoutDimension{T}
+            vals::T
+            mode::Symbol
+            colname::AbstractString
+        
+            $dimension() = new{Nothing}(nothing, :equal, nothing)
+            
+            function $dimension(x::T; mode = :equal) where T<:AbstractString
+                colname = colnames.$dimension
+                return new{T}(x, mode, colname)
+            end
+
+            function $dimension(vec::T; mode = :in) where T<:AbstractVector
+                colname = colnames.$dimension
+                return new{T}(vec, mode, colname)
+            end
+
+            function $dimension(args::T...; mode = :in) where T<:AbstractString
+                colname = colnames.$dimension
+                vec = [args...]
+                return new{typeof(vec)}(vec, mode, colname)
             end
         end
     end)
 end
 
 struct Mask
-    row
-    col
-    filters
+    row::ResultDimension
+    col::ResultDimension
+    filters::Vector{ResultDimension}
 
-    function Mask(row, col, filters...)
+    function Mask(row::ResultDimension, col::ResultDimension, filters::ResultDimension...)
         new(row, col, [filters...])
     end
 end
 
-const colname_dict = Dict(
-    :Region => "region_dispatch_",
-    :Technology => "technology_",
-    :Timestep => "timestep_superordinate_dispatch_",
-    :Carrier => "carrier_",
-    :Variable => "variable",
-    :Scenario => "scenario"
-)
-
-function get_col_name_sceleton(af::ResultDimension, p::Pair)
-    dim_type = typeof(af) |> Symbol
-    dim = p[1]
-    return  colname_dict[dim_type]*"$dim"
-end
-
-function get_col_name_sceleton(af::ResultDimension, x::AbstractString)
-    dim_type = typeof(af) |> Symbol
-    return  colname_dict[dim_type]
-end
-
-function get_col_name_sceleton(af::ResultDimension, i::Int)
-    dim_type = typeof(af) |> Symbol
-    return colname_dict[dim_type]*"$i"
-end
-
-
-function anyfilter!(df::AbstractDataFrame, af::ResultDimension)
-
-    isnothing(af.pairs) && return df
-    # check if column exists
-    cols = names(df)
-    for p in af.pairs
-        colname = get_col_name_sceleton(af, p)
-        if colname in cols
-            typeof(p) <: Pair ? (y = p[2]) : (y = p)   
-            if af.mode == :equal
-                filter!(x-> isequal(y, x[colname]), df)
-            elseif af.mode == :occursin
-                filter!(x-> occursin(y, x[colname]), df)
-            elseif af.mode == :in
-                filter!(x-> x[colname] in y, df)
-            else
-                @error "Mode '$(af.mode)' is not supported!"
-            end
-        else
-            @warn "Column $colname does not exists in DataFrame" cols
-        end
-    end
-
+function filter_df!(df::AbstractDataFrame, af::ResultDimension{Nothing})
     return df
 end
 
+
+function filter_df!(df::AbstractDataFrame, af::ResultDimension{T}) where T<:AbstractString
+
+    if af.colname in names(df)
+        if af.mode == :equal
+            filter!(x-> isequal(af.vals, x[af.colname]), df)
+        elseif af.mode == :occursin
+            filter!(x-> occursin(af.vals, x[af.colname]), df)
+        else 
+            @warn "Mode $(af.mode) is not supported for filtering based on a string" af.vals
+        end
+    else
+        @warn "Column $(af.colname) does not exists in DataFrame" names(df)
+    end
+    
+    return df
+end
+
+function filter_df!(df::AbstractDataFrame, af::ResultDimension{T}) where T<:AbstractVector
+
+    if af.colname in names(df)
+        if af.mode == :in
+            filter!(x-> x[af.colname] in af.vals, df)
+        else 
+            @warn "Mode $(af.mode) is not supported for filtering based on a vector" af.vals
+        end
+    else
+        @warn "Column $(af.colname) does not exists in DataFrame" names(df)
+    end
+    
+    return df
+end
 
 struct PivotResult
     result::AnymodResult
@@ -108,15 +157,19 @@ struct PivotResult
         df = copy(ar.summarytable)
         # filter summarytable
         for fil in m.filters
-            anyfilter!(df, fil)
+            filter_df!(df, fil)
         end
 
-        anyfilter!(df, m.row)
-        anyfilter!(df, m.col)
-        rowkeys = get_col_name_sceleton(m.row, m.row.dim)
-        colkey = get_col_name_sceleton(m.col, m.col.dim)
-        df = combine(groupby(df, [rowkeys, colkey]), "value" => sum => "value")
-        df = unstack(df, rowkeys, colkey, "value")
+        filter_df!(df, m.row)
+        filter_df!(df, m.col)
+
+        groupkeys = [m.row.colname, m.col.colname]
+        df = combine(groupby(df, groupkeys), "value" => sum => "value")
+        df = unstack(df, m.row.colname, m.col.colname, "value")
         return new(ar, m, df)
     end
+end
+
+function pivotresult(ar::AnymodResult, m::Mask)
+    return PivotResult(ar, m).df
 end
